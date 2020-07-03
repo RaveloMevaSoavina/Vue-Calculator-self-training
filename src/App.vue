75b3b1485b3515a9c5d90c749b70c446b8dd0607
Ravelo Meva Soavina <mevaravelosoavina@gmail.com>
2020-07-03 11:46:27 +0300

    [Update] :: Update appearance of all buttons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <div class="topbar">
+      <a
+        target="_blank"
+        href="https://github.com/RaveloMevaSoavina/Vue-Calculator-self-training"
+      >https://github.com/RaveloMevaSoavina/Vue-Calculator-self-training</a>
+    </div>
+    <br />
     <Calculator />
   </div>
 </template>
@@ -22,6 +29,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.topbar {
+  height: 4rem;
+  width: 100vw;
+  background-color: #2c3e50;
+  color: #fff;
+  padding: 1.2rem 0;
+}
+
+.topbar a {
+  color: #fff;
+  text-decoration: none;
+  cursor: pointer;
 }
 </style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,15 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>Hello world</h1>
+  <div class="calculator">
+    <div class="display">{{result}}</div>
+    <div class="btn">AC</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">/</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">X</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="zero btn">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Calculator"
+  name: "Calculator",
+  data() {
+    return {
+      result: "0"
+    };
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.calculator {
+  margin: 4rem auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  /* border: 1px solid red; */
+  width: 400px;
+  font-size: 2rem;
+}
+
+.display {
+  grid-column: 1/5;
+  height: 5rem;
+  background-color: #333;
+  color: #fff;
+  text-align: right;
+  font-size: 3rem;
+  padding-top: 1rem;
+}
+
+.btn {
+  background-color: #ddd;
+  border: 0.1px solid #aaa;
+}
+
+.zero {
+  grid-column: 1/3;
+  text-align: center;
+}
+
+.operator {
+  background-color: #f69332;
+}
 </style>
