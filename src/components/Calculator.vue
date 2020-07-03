c8a800977829b14529c1f3d35535dd948d5b3920
Ravelo Meva Soavina <mevaravelosoavina@gmail.com>
2020-07-03 15:26:19 +0300

    [Update] :: Update all buttons methods

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculator">
-    <div class="display">{{result}}</div>
-    <div class="btn">AC</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn operator">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">X</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn operator">+</div>
-    <div class="zero btn">0</div>
-    <div class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div class="display">{{result || '0'}}</div>
+    <div @click="clear" class="btn effacer">AC</div>
+    <div @click="signer" class="btn">+/-</div>
+    <div @click="pourcentage" class="btn">%</div>
+    <div @click="division" class="btn operator">/</div>
+    <div @click="ecrireNombre(7)" class="btn">7</div>
+    <div @click="ecrireNombre(8)" class="btn">8</div>
+    <div @click="ecrireNombre(9)" class="btn">9</div>
+    <div @click="multiplication" class="btn operator">x</div>
+    <div @click="ecrireNombre(4)" class="btn">4</div>
+    <div @click="ecrireNombre(5)" class="btn">5</div>
+    <div @click="ecrireNombre(6)" class="btn">6</div>
+    <div @click="soustraction" class="btn operator">-</div>
+    <div @click="ecrireNombre(1)" class="btn">1</div>
+    <div @click="ecrireNombre(2)" class="btn">2</div>
+    <div @click="ecrireNombre(3)" class="btn">3</div>
+    <div @click="addition" class="btn operator">+</div>
+    <div @click="ecrireNombre(0)" class="zero btn">0</div>
+    <div @click="toFloat" class="btn">.</div>
+    <div @click="egal" class="btn operator">=</div>
   </div>
 </template>
 
@@ -28,8 +28,62 @@ export default {
   name: "Calculator",
   data() {
     return {
-      result: "0"
+      result: " ",
+      opClicked: false,
+      operateur: null,
+      previous: null
     };
+  },
+  methods: {
+    clear() {
+      this.result = " ";
+      const content = document.querySelector(".effacer");
+      content.innerText = "AC";
+    },
+    signer() {
+      this.result = -this.result;
+    },
+    pourcentage() {
+      this.result = `${parseFloat(this.result) / 100}`;
+    },
+    ecrireNombre(nombre) {
+      if (this.opClicked) {
+        this.result = "";
+        this.opClicked = false;
+      }
+      const content = document.querySelector(".effacer");
+      content.innerText = "C";
+      this.result = `${this.result}${nombre}`;
+    },
+    toFloat() {
+      if (this.result.indexOf(".") === -1) {
+        this.result = `${this.result}${"."}`;
+      }
+    },
+    operatorIsClicked() {
+      this.opClicked = true;
+      this.previous = this.result;
+    },
+    division() {
+      this.operateur = (x, y) => x / y;
+      this.operatorIsClicked();
+    },
+    multiplication() {
+      this.operateur = (x, y) => x * y;
+      this.operatorIsClicked();
+    },
+    soustraction() {
+      this.operateur = (x, y) => parseFloat(x) - parseFloat(y);
+      this.operatorIsClicked();
+    },
+    addition() {
+      this.operateur = (x, y) => parseFloat(x) + parseFloat(y);
+      this.operatorIsClicked();
+    },
+    egal() {
+      this.result = `${this.operateur(this.previous, this.result)}`;
+      this.previous = null;
+    }
   }
 };
 </script>
@@ -58,6 +112,7 @@ export default {
 .btn {
   background-color: #ddd;
   border: 0.1px solid #aaa;
+  cursor: pointer;
 }
 
 .zero {
